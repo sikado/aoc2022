66fde8b06f5ea41e0221b83b4bfc7c643903b148
Julien Brun <julien@julienbrun.fr>
2022-12-07 12:02:04 +0100

    split parts in boiler specs

--- src/boiler/index.spec.ts
+++ src/boiler/index.spec.ts
@@ -2,35 +2,39 @@ import { process } from "./index";
 import { getData } from "../utils";
 
 describe("Day_X", () => {
-    test("Test case 1", () => {
-        const data = getData(__dirname + "/tcase.txt");
+    describe("Part 1", () => {
+        test("Test case", () => {
+            const data = getData(__dirname + "/tcase.txt");
 
-        const output = process(data);
+            const output = process(data);
 
-        expect(output).toEqual([""]);
+            expect(output).toEqual([""]);
+        });
+
+        test("Puzzle", () => {
+            const data = getData(__dirname + "/data.txt");
+
+            const output = process(data);
+
+            expect(output).toEqual([""]);
+        });
     });
 
-    test("Puzzle 1", () => {
-        const data = getData(__dirname + "/data.txt");
+    describe("Part 2", () => {
+        test("Test case", () => {
+            const data = getData(__dirname + "/tcase.txt");
 
-        const output = process(data);
+            const output = process(data);
 
-        expect(output).toEqual([""]);
-    });
+            expect(output).toEqual([""]);
+        });
 
-    test("Test case 2", () => {
-        const data = getData(__dirname + "/tcase.txt");
+        test("Puzzle", () => {
+            const data = getData(__dirname + "/data.txt");
 
-        const output = process(data);
+            const output = process(data);
 
-        expect(output).toEqual([""]);
-    });
-
-    test("Puzzle 2", () => {
-        const data = getData(__dirname + "/data.txt");
-
-        const output = process(data);
-
-        expect(output).toEqual([""]);
+            expect(output).toEqual([""]);
+        });
     });
 });
