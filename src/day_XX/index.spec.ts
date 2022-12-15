9553640069c2398614ecb469ecca55ec399f67d7
Julien Brun <julien@julienbrun.fr>
2022-12-15 13:04:23 +0100

    chore: :truck: rename template folder

--- src/day_XX/index.spec.ts
+++ src/day_XX/index.spec.ts
@@ -2,7 +2,7 @@ import { process } from "./index";
 import { getData } from "../utils";
 import { join } from "path";
 
-describe("Day_X", () => {
+describe("Day_XX", () => {
     describe("Part 1", () => {
         test("Test case", () => {
             const data = getData(join(__dirname, "tcase.txt"));
