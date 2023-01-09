0d09cc15fa55fa8669c5ec60894568404f4c8446
Julien Brun <julien@julienbrun.fr>
2023-01-09 13:22:24 +0100

    feat: ✨ Add P5 to React wrapper

---
+++ src/components/Sketch.tsx
@@ -0,0 +1,28 @@
+import P5 from "p5";
+import { Component, createRef } from "react";
+
+export interface SketchProps {
+    sketch: (p5: P5) => void;
+}
+
+export default class Sketch extends Component<SketchProps> {
+    private readonly canvaParent;
+    private P5Instance: P5 | null = null;
+
+    constructor(props: SketchProps) {
+        super(props);
+        this.canvaParent = createRef<HTMLDivElement>();
+    }
+
+    componentDidMount() {
+        this.P5Instance = new P5(this.props.sketch, this.canvaParent.current!);
+    }
+
+    componentWillUnmount(): void {
+        this.P5Instance?.remove();
+    }
+
+    render() {
+        return <div ref={this.canvaParent}></div>;
+    }
+}
